5128d238172dcd758c790580d677c5c46ffaaadd
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2023-01-04 14:35:11 -0600

    ADD: ngModel implemented to the inputs of the form
    This feature is used to take dato from a form

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 10000   
+  }
+
   agregar() {
-    console.log("Esto es una prueba!!");
+    console.log(this.nuevo);
   }
 }
